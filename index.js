ac5290d0dc62b82f5ab852e19c9d8c8ad8544de8
Emmanuel <haiman4real@gmail.com>
2022-12-05 10:28:25 -0500

    UPDATE

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const cors = require("cors")
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const server = http.createServer((req, res) => {
     // res.header("Access-Control-Allow-Origin", "*");
@@ -95,6 +96,102 @@ const server = http.createServer((req, res) => {
 
     else if (req.url==='/api')
     {
+
+        // const { MongoClient } = require('mongodb');
+
+        // async function main() {
+        //     /**
+        //      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+        //      * See https://docs.mongodb.com/drivers/node/ for more details
+        //      */
+        //     const uri = "mongodb+srv://haiman4real:lLGp7WPDstB4H2vi@emmaogunwobi.26qae1f.mongodb.net/newsarticles?retryWrites=true&w=majority";
+
+        //     /**
+        //      * The Mongo Client you will use to interact with your database
+        //      * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
+        //      * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
+        //      * pass option { useUnifiedTopology: true } to the MongoClient constructor.
+        //      * const client =  new MongoClient(uri, {useUnifiedTopology: true})
+        //      */
+        //     const client = new MongoClient(uri);
+
+        //     try {
+        //         // Connect to the MongoDB cluster
+        //         await client.connect();
+
+        //         // Make the appropriate DB calls
+
+        //         // Find the listing named "Infinite Views" that we created in create.js
+        //         await findOneListingByName(client, "Joe Flint");
+        //         await findAll(client);
+
+        //     } finally {
+        //         // Close the connection to the MongoDB cluster
+        //         await client.close();
+        //     }
+        // }
+
+        // main().catch(console.error);
+
+        // async function findOneListingByName(client, nameOfListing) {
+        //     // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
+        //     const result = await client.db("newsarticles").collection("allnews").findOne({ author: nameOfListing });
+
+        //     if (result) {
+        //         console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
+        //         console.log(result);
+        //     } else {
+        //         console.log(`No listings found with the name '${nameOfListing}'`);
+        //     }
+        // }
+
+        // async function findAll(client) {
+        //     // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
+        //     const cursor = await client.db("newsarticles").collection("allnews").find();
+
+        //     const results = await cursor.toArray();
+
+        //     console.log(results);
+
+        //     res.setHeader("Access-Control-Allow-Origin", "*");
+        //     res.writeHead(200, { 'Content-Type': 'application/json' });
+        //     res.write(results);
+        //     res.end();
+
+        //     // Print the results
+        //     // if (results.length > 0) {
+        //     //     console.log(`Found:`);
+        //     //     results.forEach((result, i) => {
+        //     //         const date = new Date(result.publishedAt).toDateString();
+
+        //     //         console.log();
+        //     //         console.log(`${i + 1}. name: ${result.author}`);
+        //     //         console.log(`   _id: ${result._id}`);
+        //     //         // console.log(`   bedrooms: ${result.bedrooms}`);
+        //     //         // console.log(`   bathrooms: ${result.bathrooms}`);
+        //     //         console.log(`   most recent review date: ${date}`);
+        //     //     });
+        //     // } else {
+        //     //     console.log(`No listings found`);
+        //     // }
+        // }
+
+
+        // mongodb+srv://haiman4real:<password>@emmaogunwobi.26qae1f.mongodb.net/?retryWrites=true&w=majority
+        
+        // const uri = "mongodb+srv://haiman4real:lLGp7WPDstB4H2vi@emmaogunwobi.26qae1f.mongodb.net/?retryWrites=true&w=majority";
+        // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        // client.connect(err => {
+        // const collection = client.db("newsarticles").collection("allnews");
+        // const results = collection.toArray();
+        // console.log(results);
+        // perform actions on the collection object
+            // res.setHeader("Access-Control-Allow-Origin", "*");
+            // res.writeHead(200, { 'Content-Type': 'application/json' });
+            // res.end(collection);
+        // client.close();
+        // });
+
         fs.readFile(
             path.join(__dirname, 'public', 'newsarticles.json'),'utf-8',
                     (err, content) => {
